353f2de3c346f503d8c810e38830cdef253474de
msldz <massil.benna17@gmail.com>
2026-03-25 12:01:14 +0100

    mise en place animation sur la box

--- Assets/Scripts/PlayerDebugPosition.cs
+++ Assets/Scripts/PlayerDebugPosition.cs
@@ -5,11 +5,14 @@ public class PlayerDebugPosition : MonoBehaviour
     #if UNITY_EDITOR
     [SerializeField]
     private Transform debugTransform;
+
+    [SerializeField]
+    private bool isEnabled = true;
  
  
     void Awake()
     {
-        if (debugTransform)
+        if (debugTransform && isEnabled)
         {
             transform.position = debugTransform.position;
         }
