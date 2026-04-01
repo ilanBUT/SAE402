0282abd55c170b7b9b04eae1b1d19bb3aaa3f531
Ilan.Bazonnet <ilan.bazonnet@gmail.com>
2026-04-01 10:45:27 +0200

    Mise à jour block Hit / pommes

--- Assets/Scripts/BlockHit.cs
+++ Assets/Scripts/BlockHit.cs
@@ -1,12 +1,27 @@
+using System.Collections;
 using UnityEngine;
  
 public class BlockHit : MonoBehaviour
 {
+    // Pour le cours, limiter le nombre d'interactions possible avec le bloc
+    // Et quand c'est atteint, changer l'opacité du sprite (voir ligne 15)
+    private SpriteRenderer sr;
     private Animator animator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+ 
+    [SerializeField]
+    private int maxHits = 2;
+ 
+    private int currentHits = 0;
+ 
+    private bool isAnimating = false;
+ 
+    [SerializeField]
+    private GameObject itemPrefab;
+ 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
  
     void OnCollisionEnter2D(Collision2D collision)
@@ -17,9 +32,39 @@ public class BlockHit : MonoBehaviour
         }
  
         ContactPoint2D contact = collision.GetContact(0);
-        if (contact.normal.y > 0.5f)
+ 
+        if (contact.normal.y > 0.5f && currentHits < maxHits && !isAnimating)
         {
+            currentHits = currentHits + 1;
             animator.SetTrigger("Hit");
+ 
+            if (itemPrefab != null)
+            {
+                StartCoroutine(Hit());
+            }
+ 
+            if (currentHits == maxHits)
+            {
+                sr.color = new Color(1f, 1f, 1f, 0.5f);
+            }
         }
     }
+ 
+    IEnumerator Hit()
+    {
+        isAnimating = true;
+        GameObject item = Instantiate(
+            itemPrefab,
+            transform.position,
+            Quaternion.identity
+        );
+        Collectible collectible = item.GetComponent<Collectible>();
+        yield return item.transform.MoveBackAndForth(
+            item.transform.localPosition + Vector3.up * 1.5f
+        );
+        collectible.Picked();
+        isAnimating = false;
+    }
 }
+ 
+ 
